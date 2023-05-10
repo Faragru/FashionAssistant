d882fe925a87a49464c8a4fc213ae2e3157e7ad1
Faragru <ciprians39@yahoo.com>
2023-05-10 22:32:46 +0300

    Initial Commit

---
+++ FashionAssistant/FashionAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FashionAssistantApp.swift
+//  FashionAssistant
+//
+//  Created by Stan Ciprian on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FashionAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
